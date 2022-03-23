0dd02cec27fc3c69a5b4213493ca97e6138da63d
Olga Kolikhalova <kolikolga@yandex.ru>
2022-03-23 19:30:31 +0300

    сделала переключение радио кнопки и бордера по клику

--- blocks/js/script.js
+++ blocks/js/script.js
@@ -1,22 +1,26 @@
 const radioItems = document.getElementById('radio__buttons');
 // ищем по классу ставим точку
 const radioItem = document.querySelectorAll('.radio__item');
-const radioItemBorder = document.querySelectorAll('.radio__item_border');
-const radioFake = document.querySelector('.radio__fake');
-const radioFakeAfter = document.querySelector('.radio__fake_after')
 
 console.log(radioItem); // это сами кнопки type = button nodelist
-const radioItemArray = Array.prototype.slice.call(radioItem);
+const radioItemArray = Array.prototype.slice.call(radioItem); // перевели нодлист в Array
 console.log(radioItemArray);
 
 radioItemArray.forEach((el, i) => {
    el.addEventListener('click', e => {
-      // console.log('tada');
-      console.log(el.classList);
-      console.log(el.dataset.itemid);
-
+      // console.log(el.dataset.itemid);
+      // console.log(el.classList);
    });
 
+});
+
+radioItemArray.forEach(item => {
+   item.addEventListener('click', (e) => {
+      radioItemArray.forEach(el => {
+         el.classList.remove('radio__border_red');
+      });
+      item.classList.add('radio__border_red');
+   });
 });
 
 
@@ -26,8 +30,5 @@ radioItem.forEach((button, index) => {
    // console.log(index); //выводит index каждой кнопки
    button.addEventListener('click', (e) => {
       console.log(e.target.classList);
-      if (radioFakeAfter) {
-         console.log('radio')
-      }
-   })
+   });
 }); // перебираем кнопки
