e1a95eaa4037375911382d9b569b94e3cf5b5aec
Olga Kolikhalova <kolikolga@yandex.ru>
2022-05-13 07:02:30 +0300

    переделала checkbox чекает три эелемента

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,40 +1,47 @@
 const checkbox = () => {
-   const checkboxButton = document.querySelectorAll('.checkbox__item');
+
    const checkboxSpan = document.querySelectorAll('.checkbox__span');
+   const checkboxButton = document.querySelectorAll('input[type="checkbox"]');
+   const checkBox = document.getElementById('input[type="checkbox"]');
+   console.log(checkboxButton);
 
 
-   const activeElems = [];
+   checkboxButton.forEach((item) => {
+      item.addEventListener('click', () => {
+         console.log(item)
+      })
+   })
 
-   function activeElemClick() {
-      let index = activeElems.indexOf(this);
-      if (index >= 0) { // Элемент был активным
-         this.classList.remove("radio__border");
-         activeElems.splice(index, 1);
-      } else if (activeElems.length < 3) { // Элемент не был активным и можно добавить
-         this.classList.add("radio__border");
-         activeElems.push(this);
-      }
+   let f = document;
+   f.onchange = () => {
+      let n = f.querySelectorAll('[type="checkbox"]'),
+         l = f.querySelectorAll('[type="checkbox"]:checked');
+      for (let j = 0; j < n.length; j++)
+         if (l.length >= 3) { // если отметить три и более галочки
+            n[j].disabled = true; // все чекбоксы становятся disabled
+            for (let i = 0; i < l.length; i++)
+               l[i].disabled = false; // но disabled убирается с помеченных галочками чекбоксов
+         } else {
+            n[j].disabled = false; // если выделить менее трёх галочек, то disabled снимается со всех чекбоксов
+         }
    }
-
-   for (let e of checkboxButton) {
-      e.addEventListener(`click`, activeElemClick);
-   }
-
 
    const activeElems1 = [];
+
 
    function activeElemClick1() {
       let index = activeElems1.indexOf(this);
       if (index >= 0) { // Элемент был активным
-         this.classList.remove("checkbox__span_red");
+         this.checked = false;
          activeElems1.splice(index, 1);
       } else if (activeElems1.length < 3) { // Элемент не был активным и можно добавить
-         this.classList.add("checkbox__span_red");
+         this.checked = true;
          activeElems1.push(this);
       }
    }
-   for (let e of checkboxSpan) {
+   for (let e of checkboxButton) {
       e.addEventListener(`click`, activeElemClick1);
+      console.log(e.checked)
    };
 
 };
